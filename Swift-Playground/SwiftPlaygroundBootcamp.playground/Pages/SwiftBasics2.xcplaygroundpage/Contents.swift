e35fc7ae2af929911515612bad84696f2867388e
soumya-mishra-97 <soumyamishra637@gmail.com>
2025-03-28 00:08:21 +0530

    Code updated

--- Swift-Playground/SwiftPlaygroundBootcamp.playground/Pages/SwiftBasics2.xcplaygroundpage/Contents.swift
+++ Swift-Playground/SwiftPlaygroundBootcamp.playground/Pages/SwiftBasics2.xcplaygroundpage/Contents.swift
@@ -1,3 +1,40 @@
 import Foundation
 
-/// Tuples
+/// `Tuples`
+/// If you have `multiple separate of data` so `tuples` easily `combined` them and `return` it from a `function`
+/// Usecaes: `Data grouping`
+let userDetails: (String, Int, Bool) = ("Alice", 28, true)
+print(userDetails.0) /// Alice
+print(userDetails.1) /// 28
+print(userDetails.2) /// true
+
+let person: (name: String, age: Int) = ("Bob", 30)
+print(person.name) /// Bob
+print(person.age) /// 30
+
+func getUserName() -> String{
+    return "John"
+}
+
+func getUserage() -> Int{
+    return 19
+}
+
+func isUserAbove18() -> Bool{
+    return true
+}
+
+func getUserDetails() -> (String, Int, Bool){
+    let name = getUserName()
+    let age = getUserage()
+    let isAbove18 = isUserAbove18()
+    
+    print("Name: \(name), Age: \(age), isAbove18: \(isAbove18 == true ? "Yes": "No")")
+    return (name, age, isAbove18)
+}
+
+getUserDetails()
+
+///`Defer Statement`
+/// Executed in last
+/// `Layered Defer Statement`
